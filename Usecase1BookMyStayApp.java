2bcee8d70635ffe95423dcf53540b7d85b1ce406
Ritu <linkedin31104@gmail.com>
2026-04-08 22:45:40 +0530

    Added Use Case 1

---
+++ Usecase1BookMyStayApp.java
@@ -0,0 +1,14 @@
+public class Usecase1BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("  Welcome to Hotel Booking App   ");
+        System.out.println("=================================");
+
+        System.out.println("Application Name : BookMyStay");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("\nApplication started successfully!");
+    }
+}
